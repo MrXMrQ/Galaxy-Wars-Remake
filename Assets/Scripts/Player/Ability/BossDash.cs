a4987e7de9039055c2386ac67f8fc4f0e3e1afcc
Marvin Bäumer <m.baeumee.industries@gmail.com>
2024-08-16 14:31:25 +0200

    feat: new ability
    dash from the dash boss can bought

--- Assets/Scripts/Player/Ability/BossDash.cs
+++ Assets/Scripts/Player/Ability/BossDash.cs
@@ -6,6 +6,7 @@ public class BossDash : Ability
     [SerializeField] ParticleSystem dash_particles;
     [SerializeField] float DASH_SPEED;
     [SerializeField] float DASH_DISTANCE;
+    [SerializeField] GameObject player_default_projectile;
 
     public override void Activate(GameObject parent)
     {
@@ -14,5 +15,26 @@ public class BossDash : Ability
 
         SpawnParticles(dash_particles, rigidbody.position);
         rigidbody.velocity = playerMovement.movement_direction * DASH_SPEED * DASH_DISTANCE;
+
+        Shot(rigidbody);
+    }
+
+    private void Shot(Rigidbody2D rigidbody)
+    {
+        InstanceProjectile(player_default_projectile, Vector2.left, rigidbody.position);
+        InstanceProjectile(player_default_projectile, Vector2.right, rigidbody.position);
+        InstanceProjectile(player_default_projectile, Vector2.down, rigidbody.position);
+        InstanceProjectile(player_default_projectile, Vector2.up, rigidbody.position);
+    }
+
+    private void InstanceProjectile(GameObject projectile, Vector2 move_direction, Vector2 pos)
+    {
+        ProjctileDefaultLogic projectile_script = projectile.GetComponent<ProjctileDefaultLogic>();
+
+        if (projectile_script != null)
+        {
+            projectile_script.SetMoveDirection(move_direction);
+            Instantiate(projectile, pos, Quaternion.identity);
+        }
     }
 }
